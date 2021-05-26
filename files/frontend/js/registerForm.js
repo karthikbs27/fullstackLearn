7989f3313fa7cbf8351f156114e7308215050389
karthikbs27 <karthik2000tbt@gmail.com>
2021-05-26 17:56:38 +0530

    ajax working

--- files/frontend/js/registerForm.js
+++ files/frontend/js/registerForm.js
@@ -8,7 +8,26 @@ function validatepass(){
         document.getElementById('invalid').style.display = "block"
 }
 
+
+function next(){
+    x = document.getElementsByClassName("input-field")
+    go = 1
+
+    for(i = 0; i < 6 ; i++){
+        if(x[i].value == "" ) go = 0
+    }
+
+    if(go == 1){
+        
+        passformappear()
+        document.getElementById("invalid1").style.display = "none"    
+    }    
+    else{
+        document.getElementById("invalid1").style.display = "block"
+    }
+}
+
 function passformappear(){
-    document.getElementsByClassName("register").style.left = "-50%"
-    document.getElementsByClassName('password').style.left = "50%"
+    document.getElementById("reg").style.left = "-50%"
+    document.getElementById('pas').style.left = "50%"
 }
